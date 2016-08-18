1a5e0939ead5b87863b6761c2f6a3a1f7e08792a
Somni <somni@isibboi.de>
2016-08-18 11:45:24 +0200

    Added update script

---
+++ update.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Nightline Updater</title>
+    <meta name="description" content="Update the NL website">
+  </head>
+  <body>
+    <form action="update.php">
+      <input type="hidden" name="pull" value="true" />
+      <input type="submit" value="Update Website"/>
+    </form>
+
+    <?php
+      if ($_GET['pull'] == true) {
+        exec("git pull -v /srv/www/htdocs/hosting/nightline 2>&1", $output, $return_var);
+        
+        if ($return_var == 0) {
+          echo "<span style='color: green;'>Command completed successfully!</span>";
+        } else {
+          echo "<span style='color: red;'>Error $return_var!</span>";
+        }
+
+	echo "<br />\n";
+
+        foreach ($output as $line) {
+          echo "    $line<br />\n";
+        }
+      }
+    ?>
+  </body>
+</html>
