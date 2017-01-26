bba5645f74a094162d28986f7ae08315b2f7d6ca
NLKA <karlsruhe@nightlines.eu>
2017-01-26 09:55:58 +0100

    Disable tracking

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 	</footer>
 	
 	<!-- Piwik -->
-	<script type="text/javascript">
+	<!--<script type="text/javascript">
   		var _paq = _paq || [];
   		// tracker methods like "setCustomDimension" should be called before "trackPageView"
 		_paq.push(['disableCookies']);
@@ -112,7 +112,7 @@
     			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   		})();
-	</script>
+	</script>-->
 	<!-- End Piwik Code -->
 </body>
 </html>
