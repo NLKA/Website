2df6edebe2b8ba6e70e17dae617c48f4f7c20a56
Sebastian Schmidt <isibboi@gmail.com>
2017-05-02 13:32:25 +0200

    Added database example.

--- anfordern.php
+++ anfordern.php
@@ -1,5 +1,34 @@
 <?php
 
+// DB example
+include('/etc/apache2/db-passwords/nightline.php');
+
+$data = [1, 5, 9];
+
+$db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+$db->query("CREATE TABLE test (id INT PRIMARY KEY)");
+$insert_number_statement = $db->prepare("INSERT INTO test VALUES (?)");
+
+foreach ($data as $n) {
+	$insert_number_statement->execute([$n]);
+}
+
+$results = $db->query("SELECT * FROM test");
+
+foreach ($results as $row) {
+	echo implode(" -- ", $row);
+	echo "<br />";
+}
+
+$db->query("DROP TABLE test");
+
+echo "Success";
+
+/////////////////////////////////
+// TODO REMOVE !!! //////////////
+exit(0); ////////////////////////
+/////////////////////////////////
+
 // send mail
 $to      = 'karlsruhe@nightlines.eu';
 $subject = '[NL] Telefondienst angefordert';
