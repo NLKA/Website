bde961c34ef29ef748e175f42077ceb1bb5d59f0
Robin Schnaidt <rs@robinschnaidt.com>
2017-05-11 18:28:58 +0200

    Reindent code

--- demand.php
+++ demand.php
@@ -3,14 +3,14 @@
 include_once('/etc/apache2/db-passwords/nightline.php');
 
 // Write demand to db
-	$sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+$sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
-	$stmt = $sqlConnetion->prepare("INSERT INTO onDemandEntry (ip) VALUES (?)");
-	$stmt->bind_param('s', $_SERVER['REMOTE_ADDR']);
-	$stmt->execute();
-	$stmt->close();
+$stmt = $sqlConnetion->prepare("INSERT INTO onDemandEntry (ip) VALUES (?)");
+$stmt->bind_param('s', $_SERVER['REMOTE_ADDR']);
+$stmt->execute();
+$stmt->close();
+$sqlConnetion->close();
 
-	$sqlConnetion->close();
+header("Location: on-demand.html?success=1");
 
-	header("Location: on-demand.html?success=1");
 ?>
