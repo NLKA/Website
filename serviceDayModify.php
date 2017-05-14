ded44d46ec8b729b9559594a83c4a5e78f7f7454
Robin Schnaidt <rs@robinschnaidt.com>
2017-05-14 15:16:30 +0200

    Add date modification

---
+++ serviceDayModify.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once('/etc/apache2/db-passwords/nightline.php');
+
+// Check permission to view
+$tokenHash = "0c7c36061cc3c9027fcfecde263e229ef718ae66835ed88348a50f12b966e70c";
+if (hash("sha256", $_GET['token']) != $tokenHash) {
+	echo "Permission token invalid";
+	exit;
+}
+
+// Execute op on db
+$sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+if ($_GET['op'] == "delete") {
+	$stmt = $sqlConnetion->prepare("DELETE FROM serviceDay WHERE serviceDayId = ?;");
+	$stmt->bind_param('s', $_GET['id']);
+	$stmt->execute();
+	$results = $stmt->get_result();
+	$stmt->close();
+}
+
+if ($_GET['op'] == "add") {
+	$stmt = $sqlConnetion->prepare("INSERT INTO serviceDay (date) VALUES (?)");
+    $stmt->bind_param('s', STR_TO_DATE($_GET('date'), '%d-%m-%Y'));
+    $stmt->execute();
+    $stmt->close();
+}
+
+$sqlConnetion->close();
+
+// Redireact back
+header("Location: serviceDays.php?token=".$_GET['token']);
+
+?>
---
+++ serviceDays.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once('/etc/apache2/db-passwords/nightline.php');
+
+// Check permission to view
+$tokenHash = "0c7c36061cc3c9027fcfecde263e229ef718ae66835ed88348a50f12b966e70c";
+if (hash("sha256", $_GET['token']) != $tokenHash) {
+	echo "Permission token invalid";
+	exit;
+}
+
+// Fetch table from db
+$sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+
+$stmt = $sqlConnetion->prepare("SELECT * FROM serviceDay;");
+$stmt->execute();
+$results = $stmt->get_result();
+$stmt->close();
+
+$sqlConnetion->close();
+
+// Print table
+echo "<table>";
+while ($row = $results->fetch_assoc()) {
+	echo "<tr>";
+    echo "<td>".$row['serviceDayId']."</td>";
+    echo "<td>".$row['date']."</td>";
+    echo "<td>".$row['service']."</td>";
+    echo "<td><a href='serviceDayModify.php?token=".$_GET['token']."&op=delete&id=".$row['serviceDayId']."'>Delete</a></td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+?>
