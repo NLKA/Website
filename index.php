c2a22e106b3313ec551382fff9fc26c2f955d7a1
Robin Schnaidt <rs@robinschnaidt.com>
2017-05-14 17:47:17 +0200

    Re-add topbars

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
     // Check if there are scheduled services in the future or today that are bookable
     $sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
-    $stmt = $sqlConnetion->prepare("SELECT * FROM serviceDay WHERE date >= CURDATE() AND service = 0;");
+    $stmt = $sqlConnetion->prepare("SELECT * FROM serviceDay WHERE date >= CURDATE() AND service = 0 ORDER BY date ASC;");
     $stmt->execute();
     $results = $stmt->get_result();
     $stmt->close();
@@ -67,7 +67,11 @@
     // ...and display topbar if the case
     if ($results->num_rows > 0) {
       $firstRow = $results->fetch_assoc();
-      echo "<a>".$firstRow['date']."</a>";
+      if ($firstRow['date'] == date('Y-m-d')) {
+        echo "<div id='topBar'><p id='topbarText'>☎️ Wir können heute 21-0h für dich erreichbar sein: <a href='on-demand.html' id='anfordern'>Telefondienst anfordern</a></div>";
+      } else {
+        echo "<div id='topBar'><p id='topbarText'>☎️ Wir können bald 21-0h für dich erreichbar sein: <a href='on-demand.html' id='anfordern'>Telefondienst anfordern</a></div>";
+      }
     }
   ?>
 
