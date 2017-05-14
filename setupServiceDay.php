3c69d826ffdeab944c0362408446b83b115be826
Robin Schnaidt <rs@robinschnaidt.com>
2017-05-14 14:25:32 +0200

    Add service boolean

--- setupServiceDay.php
+++ setupServiceDay.php
@@ -8,6 +8,7 @@ $sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 $queryTable = "CREATE TABLE IF NOT EXISTS `serviceDay` (
 					`serviceDayId` INT NOT NULL AUTO_INCREMENT,
 					`date` date NOT NULL,
+					`service` boolean,
 					PRIMARY KEY (serviceDayId)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
 
