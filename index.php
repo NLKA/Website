3e7ea93c2e50ab0cd8d07a4a1a2d6970a7cc52a8
Robin Schnaidt <rs@robinschnaidt.com>
2017-05-14 17:55:24 +0200

    Remove debug output

--- index.php
+++ index.php
@@ -65,7 +65,6 @@
     $sqlConnetion->close();
 
     // ...and display topbar if the case
-    echo $results->num_rows;
     if ($results->num_rows > 0) {
       $firstRow = $results->fetch_assoc();
       if ($firstRow['date'] == date('Y-m-d')) {
