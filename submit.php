c8db68d8118c9fdb98459f7d0c3d59a3d2a0f583
Robin Schnaidt <rs@robinschnaidt.com>
2017-06-02 16:21:34 +0200

    Add hashing ips

--- submit.php
+++ submit.php
@@ -27,7 +27,7 @@ if (isset($_POST['story'])) {
 	$sqlConnetion = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
 	$stmt = $sqlConnetion->prepare("INSERT INTO storySubmission (story, ip) VALUES (?, ?)");
-	$stmt->bind_param('ss', $_POST['story'], $_SERVER['REMOTE_ADDR']);
+	$stmt->bind_param('ss', $_POST['story'], hash("sha256", $_SERVER['REMOTE_ADDR']));
 	$stmt->execute();
 	$stmt->close();
 
