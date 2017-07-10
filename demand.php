1f45784ef20a6b89e8f87985ac68cfb483409819
Robin Schnaidt <rs@robinschnaidt.com>
2017-07-10 14:17:52 +0200

    Add missing staff check

--- demand.php
+++ demand.php
@@ -11,37 +11,51 @@ if ($_GET['origin'] == "onDemandButton") {
     $results = $stmt->get_result();
     $stmt->close();
 
-    if ($results->num_rows > 0) {
+    $onDemandDayAvailable = $results->num_rows > 0;
+    if ($onDemandDayAvailable) {
         $firstRow = $results->fetch_assoc();
 
-        $dateOutputString = "n/a";
-        if ($firstRow['date'] == date('Y-m-d')) {
-            $dateOutputString = "heute";
+        // Check if at least two nightliners are available
+        $stmt = $sqlConnetion->prepare("SELECT user FROM serviceDayStaff WHERE serviceDayId = ?");
+        $stmt->bind_param('i', $firstRow['serviceDayId']);
+        $stmt->execute();
+        $resultsUsers = $stmt->get_result();
+        $stmt->close();
+
+        $serviceStaffAvailable = $resultsUsers->num_rows >= 2;
+            if ($serviceStaffAvailable) {
+                $dateOutputString = "n/a";
+            if ($firstRow['date'] == date('Y-m-d')) {
+                $dateOutputString = "heute";
+            } else {
+                $date = new DateTime($firstRow['date']);
+                $dateOutputString = $date->format('d.m.');
+            }
+
+            // Write to db
+            $stmt = $sqlConnetion->prepare("INSERT INTO onDemandEntry (ip, serviceDayId) VALUES (?,?)");
+            $stmt->bind_param('si', hash("sha256", $_SERVER['REMOTE_ADDR']), $firstRow['serviceDayId']);
+            $stmt->execute();
+            $stmt->close();
+
+            echo "write to db";
+
+            // Send mail
+            $to      =  'ka-aktive@nl2.kip.uni-heidelberg.de';
+            $subject =  '[NL-Bot] Dienst angefordert';
+            $message =  'Es wurde eben ein Telefondienst für '.$dateOutputString.' angefordert. Einen schönen Tag dir! ☀️  -- Nightline Bot';
+            $headers =  'From: no-reply@nightline-karlsruhe.de'."\r\n".
+                        'Reply-To: no-reply@nightline-karlsruhe.de'."\r\n".
+                        'X-Mailer: PHP/'.phpversion();
+
+            mail($to, $subject, $message, $headers);
+
+            // Redirect back
+            header("Location: on-demand.html?success=1");
         } else {
-            $date = new DateTime($firstRow['date']);
-            $dateOutputString = $date->format('d.m.');
+            // Redirect back
+            header("Location: on-demand.html");
         }
-
-        // Write to db
-		$stmt = $sqlConnetion->prepare("INSERT INTO onDemandEntry (ip, serviceDayId) VALUES (?,?)");
-		$stmt->bind_param('si', hash("sha256", $_SERVER['REMOTE_ADDR']), $firstRow['serviceDayId']);
-		$stmt->execute();
-		$stmt->close();
-
-        echo "write to db";
-
-		// Send mail
-		$to      = 	'ka-aktive@nl2.kip.uni-heidelberg.de';
-		$subject = 	'[NL-Bot] Dienst angefordert';
-		$message = 	'Es wurde eben ein Telefondienst für '.$dateOutputString.' angefordert. Einen schönen Tag dir! ☀️  -- Nightline Bot';
-		$headers = 	'From: no-reply@nightline-karlsruhe.de'."\r\n".
-         			'Reply-To: no-reply@nightline-karlsruhe.de'."\r\n".
-           			'X-Mailer: PHP/'.phpversion();
-
-		mail($to, $subject, $message, $headers);
-
-		// Redirect back
-		header("Location: on-demand.html?success=1");
     } else {
         // Redirect back
         header("Location: on-demand.html");
