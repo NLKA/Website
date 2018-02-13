954a069c7b1706dfdca467ef2281161021b6ac16
Robin Schnaidt <rs@robinschnaidt.com>
2018-02-13 12:21:05 +0100

    Fix leader board dummy exclusion

--- dienstplan/leaderBoard.php
+++ dienstplan/leaderBoard.php
@@ -11,7 +11,7 @@ function buildLeaderBoard($pUser) {
 	echo "<h2 id='leaderBoard'>Highscore</h2>";
 
 	$sqlConnetion = User::connect();
-    $stmt = $sqlConnetion->prepare("SELECT user, COUNT(*) AS score FROM serviceDayStaff GROUP BY user ORDER BY score desc");
+    $stmt = $sqlConnetion->prepare("SELECT user, COUNT(*) AS score FROM serviceDayStaff WHERE user NOT LIKE 'DummyUser' GROUP BY user ORDER BY score desc");
     $stmt->execute();
     $results = $stmt->get_result();
     $stmt->close();
@@ -21,7 +21,7 @@ function buildLeaderBoard($pUser) {
 
     // Proceed until count 2 + 1
     while (($row = $results->fetch_assoc())  && $count <= 2) {
-        if ($lastScore != $row['score'] && $row['user'] != "DummyUser" && $lastScore != -1) {
+        if ($lastScore != $row['score'] && $lastScore != -1) {
             $count++;
         }
 
