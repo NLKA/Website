6901f8fbe144222a9d6123c745e95a4a58fc3d65
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2018-10-11 18:10:47 +0200

    Improve version output in footer

--- dienstplan/footer.php
+++ dienstplan/footer.php
@@ -3,6 +3,6 @@
 require_once('version.php');
 global $versionString;
 
-$footer = "<p id='footer'>v".$versionString."Nightline Karlsruhe ☀️ 2018. <a href='../impressum.html'>Impressum</a><br/>Fragen, Anregungen? An Robin. Diskutieren: <a href='mailto:ka-aktive@nl2.kip.uni-heidelberg.de'>ka-aktive@nl2.kip.uni-heidelberg.de</a></p>";
+$footer = "<p id='footer'>Dienstplan v".$versionString.", Nightline Karlsruhe ☀️ 2018. <a href='../impressum.html'>Impressum</a><br/>Fragen, Anregungen? An Robin. Diskutieren: <a href='mailto:ka-aktive@nl2.kip.uni-heidelberg.de'>ka-aktive@nl2.kip.uni-heidelberg.de</a></p>";
 
 ?>
