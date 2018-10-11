526f171b7e4820f64fe483906bffa7d079d6a00c
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2018-10-11 17:33:28 +0200

    Add only displaying discarded email address

--- dienstplan/usersTable.php
+++ dienstplan/usersTable.php
@@ -31,7 +31,8 @@ function buildUsersTable($pUser) {
             echo "</td>";
             
             echo "<td>";
-                echo "<p>".$row['email']."</p>";
+                $discardedEmailAddress = ($row['email'].charAt(0)).($row['email'].charAt(1))."xxxx@xxxxx.xx";
+                echo "<p>".$discardedEmailAddress."</p>";
                 echo "<p><a class='yellowButton' href='passwordReset.php?user=".$row['user']."'>Passwort zurücksetzen</a></p>";
             echo "</td>";
 
