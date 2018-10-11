39ecc708a7dc7e89d4d832c8575da2018c264550
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2018-10-11 18:09:24 +0200

    Add global version string and version output

--- dienstplan/footer.php
+++ dienstplan/footer.php
@@ -1,5 +1,8 @@
 <?php
 
-$footer = "<p id='footer'>Nightline Karlsruhe ☀️ 2018. <a href='../impressum.html'>Impressum</a><br/>Fragen, Anregungen? An Robin. Diskutieren: <a href='mailto:ka-aktive@nl2.kip.uni-heidelberg.de'>ka-aktive@nl2.kip.uni-heidelberg.de</a></p>";
+require_once('version.php');
+global $versionString;
+
+$footer = "<p id='footer'>v".$versionString."Nightline Karlsruhe ☀️ 2018. <a href='../impressum.html'>Impressum</a><br/>Fragen, Anregungen? An Robin. Diskutieren: <a href='mailto:ka-aktive@nl2.kip.uni-heidelberg.de'>ka-aktive@nl2.kip.uni-heidelberg.de</a></p>";
 
 ?>
---
+++ dienstplan/version.php
@@ -0,0 +1,5 @@
+<?php
+
+$versionString = "1.1";
+
+?>
