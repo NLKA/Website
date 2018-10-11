e2cac197c13e667c6cb1d46aff9e478c1326a95e
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2018-10-11 18:21:47 +0200

    Add change log, update version

--- dienstplan/version.php
+++ dienstplan/version.php
@@ -1,5 +1,12 @@
 <?php
 
-$versionString = "1.1";
+// Current version, human readable
+$versionString = "1.1.1";
+
+// Short change log
+// - - - - - - - - -
+// 1.0: Initial system, only (on demand) service planning
+// 1.1: Public calendar system
+// 1.1.1: Restrict data output for admins, improve user table modes
 
 ?>
