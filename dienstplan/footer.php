d5a6fe9dd977ecb7183d9118e450fe58b66f689e
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2018-10-11 16:58:11 +0200

    Update footer

--- dienstplan/footer.php
+++ dienstplan/footer.php
@@ -1,5 +1,5 @@
 <?php
 
-$footer = "<p id='footer'>Robin Schnaidt für Nightline Karlsruhe ☀️ 2017. <a href='../impressum.html'>Impressum</a><br/>Fragen, Anregungen? An Robin. Diskutieren: <a href='mailto:ka-aktive@nl2.kip.uni-heidelberg.de'>ka-aktive@nl2.kip.uni-heidelberg.de</a></p>";
+$footer = "<p id='footer'>Nightline Karlsruhe ☀️ 2018. <a href='../impressum.html'>Impressum</a><br/>Fragen, Anregungen? An Robin. Diskutieren: <a href='mailto:ka-aktive@nl2.kip.uni-heidelberg.de'>ka-aktive@nl2.kip.uni-heidelberg.de</a></p>";
 
 ?>
