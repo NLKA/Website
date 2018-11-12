d55ecf3bf936ed293ec80d186923fb3a46f1eccc
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2018-11-12 15:41:48 +0100

    Upate version

--- dienstplan/version.php
+++ dienstplan/version.php
@@ -1,7 +1,7 @@
 <?php
 
 // Current version, human readable
-$versionString = "1.1.2";
+$versionString = "1.1.3";
 
 // Short change log
 // - - - - - - - - -
@@ -9,5 +9,6 @@ $versionString = "1.1.2";
 // 1.1: Public calendar system
 // 1.1.1: Restrict data output for admins, improve user table modes
 // 1.1.2: Every account type can confirm services
+// 1.1.3: Restrict saved email addresses to hashes (i.e. prefixes of hashed, due to db scheme)
 
 ?>
