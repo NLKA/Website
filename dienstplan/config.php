dbccd6b09b9579322c1f60f96ce4f70e9b0ace3e
Robin Schnaidt <20536559+rssch@users.noreply.github.com>
2019-05-06 10:23:23 +0200

    Remove dummy config data and replace with stored production credentials

--- dienstplan/config.php
+++ dienstplan/config.php
@@ -5,10 +5,10 @@ include_once('/etc/apache2/db-passwords/nightline.php');
 $allowCreateOriginAccount = false;
 
 // MySQL db data
-$dbName = "serviceDaysSystem";		// Name of SQL database which will be created
-$dbServer = "localhost:8889";
-$dbUser = "root";				
-$dbPassword = "root";
+$dbName = $DB_NAME;		// Name of SQL database which will be created
+$dbServer = $DB_HOST;
+$dbUser = $DB_USER;				
+$dbPassword = $DB_PASSWORD;
 $dbSalt = '$6$3BS49wgmW3yFEiuqQ/odfYItivM=';    // Change everything after $6$
 
 // Session
